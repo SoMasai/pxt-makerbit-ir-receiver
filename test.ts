e1f06d86f20c8fb878cd5ca51b0bbda185d58158
SoMasai <42625986+SoMasai@users.noreply.github.com>
2021-10-30 22:06:12 +0900

    changetest

--- test.ts
+++ test.ts
@@ -3,10 +3,10 @@
  */
 
 makerbit.connectIrReceiver(DigitalPin.P0, IrProtocol.NEC);
-makerbit.onIrButton(IrButton.Ok, IrButtonAction.Pressed, () => {});
-makerbit.onIrButton(IrButton.Up, IrButtonAction.Released, () => {});
+makerbit.onIrButton(IrButton.B0_0, IrButtonAction.Pressed, () => {});
+makerbit.onIrButton(IrButton.B1_0, IrButtonAction.Released, () => {});
 makerbit.onIrDatagram(() => {});
 const received: boolean = makerbit.wasIrDataReceived();
 const button: number = makerbit.irButton();
 const datagram: string = makerbit.irDatagram();
-const buttonCode: number = makerbit.irButtonCode(IrButton.Number_9);
+const buttonCode: number = makerbit.irButtonCode(IrButton.B2_2);
